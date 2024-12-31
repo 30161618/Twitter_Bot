2af7776e9a6f73e1a784d27ffd95571026187278
k3v <koyugi902@gmail.com>
2024-12-31 11:38:18 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,18 @@
 RSS_FEEDS = [
   'https://techcrunch.com/feed/',
     'https://www.theverge.com/rss/index.xml',
-    'https://www.cnet.com/rss/news/'
+    'https://www.cnet.com/rss/news/',
+    "https://krebsonsecurity.com/feed/",
+    "https://threatpost.com/feed/",
+    "https://www.darkreading.com/rss_simple.asp",
+    "https://www.cisa.gov/uscert/ncas/alerts.xml",
+    "https://isc.sans.edu/rssfeed.xml",
+    "https://www.schneier.com/blog/atom.xml",
+    "https://www.cvedetails.com/rss.php",
+    "https://www.exploit-db.com/rss.xml",
+    "https://www.fireeye.com/blog/threat-research/_jcr_content.feed",
+    "https://symantec-enterprise-blogs.security.com/blogs/threat-intelligence/feed "https://www.eff.org/rss/updates.xml",
+    "https://privacyinternational.org/rss.xml"
 ]
 
 GENERATED_HASHTAGS = {
