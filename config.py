cfe7b588c149e907397e8d686bea2c0638fd2142
k3v <koyugi902@gmail.com>
2024-12-31 11:33:29 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,31 @@
+# config.py
+RSS_FEEDS = [
+  'https://techcrunch.com/feed/',
+    'https://www.theverge.com/rss/index.xml',
+    'https://www.cnet.com/rss/news/'
+]
+
+GENERATED_HASHTAGS = {
+    "artificialintelligence": "#ArtificialIntelligence",
+    "ai": "#AI",
+    "machinelearning": "#DataDrivenAI",
+    "deeplearning": "#NeuralInnovations",
+    "datascience": "#InsightfulData",
+    "nlp": "#LanguageTech",
+    "technology": "#FutureTech",
+    "innovation": "#InnovateToday",
+    "digitaltransformation": "#DigitalShift",
+    "cybersecurity": "#SecureTech",
+    "robotics": "#RoboticFuture",
+    "automation": "#AutomateEverything",
+    "metaverse": "#VirtualWorlds",
+    "technews": "#TechUpdate2024",
+    "generativeai": "#CreativeAI",
+    "aiethics": "#EthicalAI",
+    "aiforgood": "#AIForChange",
+    "future": "#TechFuture",
+    "coding": "#CodeLife",
+    "programming": "#DevCommunity",
+    "developer": "#TechCreators",
+    "latest gadgets": "#Gadgets"
+}
